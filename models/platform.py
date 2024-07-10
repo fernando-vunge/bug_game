ce31ab62e0f388bcc757bf8ed7cc6f7e816df0c6
Fernando Júlio Vunge <developervunge@gmail.com>
2024-07-10 13:22:44 +0100

    implementei os vectore de temanho e posicao daplataforma

--- models/platform.py
+++ models/platform.py
@@ -1,9 +1,14 @@
 import pygame
+from models.macros import *
+
+vector = pygame.math.Vector2
 
 class Platform(pygame.sprite.Sprite):
     def __init__(self, ) -> None:
         super().__init__()
-        self.surf = pygame.Surface((150, 20))
+        self.size = vector(WIDTH, 40)
+        self.position = vector(WIDTH / 2 ,HEIGHT - int(self.size.y / 2))
+        self.surf = pygame.Surface(self.size)
         self.surf.fill((255, 0, 0))
         self.rect = self.surf.get_rect(center = (225, 200))
 
